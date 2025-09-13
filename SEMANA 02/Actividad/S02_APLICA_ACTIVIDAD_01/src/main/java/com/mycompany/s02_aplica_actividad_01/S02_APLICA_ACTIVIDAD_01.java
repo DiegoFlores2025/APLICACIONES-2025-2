a2341959da7a204cfd8f4762b1ef2f419ba988c6
DiegoFlores2025 <t01286e@ms.upla.edu.pe>
2025-09-13 12:58:21 -0500

    Add files via upload

---
+++ SEMANA 02/Actividad/S02_APLICA_ACTIVIDAD_01/src/main/java/com/mycompany/s02_aplica_actividad_01/S02_APLICA_ACTIVIDAD_01.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.s02_aplica_actividad_01;
+
+import Vista.AplicaDescuentoLapiceros;
+
+/**
+ *
+ * @author USER 17
+ */
+public class S02_APLICA_ACTIVIDAD_01 {
+
+    public static void main(String[] args) {
+        AplicaDescuentoLapiceros lapicero = new AplicaDescuentoLapiceros();
+        lapicero.setVisible(true);
+    }
+}
---
+++ SEMANA 02/Actividad/S02_APLICA_ACTIVIDAD_02/src/main/java/com/mycompany/s02_aplica_actividad_02/S02_APLICA_ACTIVIDAD_02.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.s02_aplica_actividad_02;
+
+import Vista.AplicaSueldos;
+
+/**
+ *
+ * @author USER 17
+ */
+public class S02_APLICA_ACTIVIDAD_02 {
+
+    public static void main(String[] args) {
+        AplicaSueldos sueldos = new AplicaSueldos();
+        sueldos.setVisible(true);
+    }
+}
